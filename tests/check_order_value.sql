cecde2b54e05211ae8cd84dd52ff209efc7d4cf0
Sushant Saini <sushantsaini730@gmail.com>
2026-09-11 06:41:03 +0000

    produced error for job ci

--- tests/check_order_value.sql
+++ tests/check_order_value.sql
@@ -1,1 +1,2 @@
-select order_id, total_price from {{ref('stg_orders')}} where (total_price <= 850)
+select order_id, total_price 
+from {{ref('stg_orders')}} where (total_price <= {{ var('v_min_order') }})
